fde6f8b3bc85fea2b4ea3117a9b60370f58480af
Thiago Martins <thiago.martins1112@gmail.com>
2025-03-28 18:58:33 -0300

    feat: adiciona dependência lombok, fazendo a criação automática de getters e setters

--- lab/HostelApp/src/main/java/org/example/Guest.java
+++ lab/HostelApp/src/main/java/org/example/Guest.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
 public class Guest {
 
     private String title;
@@ -11,78 +16,4 @@ public class Guest {
     private String state;
     private String country;
     private String zipCode;
-
-    public String getTitle(){
-
-        return title;
-    }
-
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
-    }
-
-    public void setLastName(String lastName){
-        this.lastName = lastName;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public String getPhone(){
-        return phone;
-    }
-
-    public void setPhone(String phone){
-        this.phone = phone;
-    }
-
-    public String getCity(){
-        return city;
-    }
-
-    public void setCity(String city){
-        this.city = city;
-    }
-
-    public String getState(){
-        return state;
-    }
-
-    public void setState(String state){
-        this.state = state;
-    }
-
-    public String getCountry(){
-        return country;
-    }
-
-    public void setCountry(String country){
-        this.country = country;
-    }
-
-    public String getZipCode(){
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode){
-        this.zipCode = zipCode;
-    }
-
 }
